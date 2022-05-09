8a65a4e40dd6509721ef084c0e5a1247e9812ee7
bouzianes <fcbdreams1991@gmail.com>
2022-05-09 11:56:33 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+ function newImage (url, left, bottom) {
+    let object = document.createElement('img')
+    object.src =  url 
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+ }
+newImage('assets/green-character.gif', 100, 100) 
+newImage('assets/pine-tree.png', 450, 200) 
+newImage( 'assets/tree.png', 200, 300)
+newImage(' assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+function newItem (url, left, bottom) {
+   let object = document.createElement('img')
+    object.src =  url 
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    object.addEventListener('dblclick', function(){
+       object.remove()
+    })
+
+}
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+  
+
+
+
+ 
+
+
+    
+
+
+
+
+   
+  
